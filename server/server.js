2485041c103a7b62b7f5b7059fb54be168ad5541
stevematdavies <stevematdavies@gmail.com>
2019-01-11 19:38:31 +0200

    adding static hosting logic

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const router = express.Router();
 const app = express();
 const port = process.env.PORT || 8080;
@@ -11,6 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes(router));
 
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+  
 app.listen(port, ()=> {
     console.log('App up and running on port:' + port);
 });
