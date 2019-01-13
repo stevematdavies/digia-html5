30e33fecc81e6cb1c702c776dddb047ae9a2e478
stevematdavies <stevematdavies@gmail.com>
2019-01-13 20:03:20 +0200

    removng dev dependencies for build

---
+++ build/precache-manifest.8c35986ca27cf0ac76dd2c8b8f63ec6d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "23db3a5c4808db308ecb",
+    "url": "/static/css/main.72bd03a1.chunk.css"
+  },
+  {
+    "revision": "23db3a5c4808db308ecb",
+    "url": "/static/js/main.23db3a5c.chunk.js"
+  },
+  {
+    "revision": "057c01a6e6ba591ccd7b",
+    "url": "/static/js/1.057c01a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a188c2f768ce5033d3f5d47be7280e25",
+    "url": "/static/media/foundation-icons.a188c2f7.woff"
+  },
+  {
+    "revision": "92827f088b9eda87169bdc2b9888ce52",
+    "url": "/static/media/foundation-icons.92827f08.eot"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/static/media/foundation-icons.e20945d7.ttf"
+  },
+  {
+    "revision": "17cb1ed2e8467b51bb26cf017daa9722",
+    "url": "/static/media/foundation-icons.17cb1ed2.svg"
+  },
+  {
+    "revision": "6869f7a8c899df3c5120f97ff25e7f4e",
+    "url": "/index.html"
+  }
+];
